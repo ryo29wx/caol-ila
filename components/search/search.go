94fb09442af6736a02a9dacbbbed1c5c05b2898c
ryo29wx <59174187+ryo29wx@users.noreply.github.com>
2023-11-17 18:06:51 +0900

    delete/ unnecessary files

--- components/search/search.go
+++ components/search/search.go
@@ -2,20 +2,19 @@ package main
 
 import (
 	"context"
-	"database/sql"
-	"fmt"
+	"encoding/json"
 	"log"
 	"net"
 	"net/http"
 	"os"
-
 	"strconv"
 
-	_ "github.com/go-sql-driver/mysql"
+	meilisearch "github.com/meilisearch/meilisearch-go"
 	pb "github.com/mockten/mockten_interfaces/searchitem"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
 )
 
@@ -23,31 +22,27 @@ const (
 	port = ":50051"
 )
 
-/**
-*initialize
- */
+var (
+	searchReqCount = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "search_req_total",
+		Help: "Total number of requests that have come to search-item",
+	})
+
+	searchResCount = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "search_res_total",
+		Help: "Total number of response that send from serch-item",
+	})
+
+	logger      *zap.Logger
+	meiliclient *meilisearch.Client
+)
+
 type server struct {
 	pb.UnimplementedSearchItemsServer
 }
 
-// Implement SearchItemServer
-func (s *server) Search(ctx context.Context, in *pb.Search) (*pb.SearchResponse, error) {
-	// logging request log
-	logger.Debug(getRequestLog(in.GetProductName(), in.GetSellerName(), in.GetExhibitionDate(), in.GetUpdateDate()))
-
-	// prometheus-exporter server
-	searchReqCount.Inc()
-	countReqs()
-
-	// Connect DB
-	db, err := connectDB()
-	if err != nil {
-		return nil, err
-	}
-
-	// close
-	defer db.Close()
-
+// Implement SearchItemServer using protocol buffer
+func (s *server) SearchItem(ctx context.Context, in *pb.GetSearchItem) (*pb.SearchResponse, error) {
 	productNameForSearch := in.GetProductName()
 	sellerNameForSearch := in.GetSellerName()
 	exhibitionDateForSearch := in.GetExhibitionDate()
@@ -56,42 +51,47 @@ func (s *server) Search(ctx context.Context, in *pb.Search) (*pb.SearchResponse,
 	rankingFilterForSearch := in.GetRankingFilter()
 	pageForSearch := in.GetPage() //int32
 
-	//Get SellerIDs
-	sellerIDs := make([]string, 0)
-	if len(sellerNameForSearch) > 0 {
-		sellerIDs, err = getSellerID(sellerNameForSearch, db)
-		if err != nil {
-			return nil, err
+	// logging request log
+	logger.Info("Request log", zap.String("productname", productNameForSearch),
+		zap.String("sellername", sellerNameForSearch),
+		zap.String("exhibitiondate", exhibitionDateForSearch),
+		zap.String("updatedate", updateDateForSearch),
+		zap.String("category", categoryForSearch),
+		zap.Int32("ranking", rankingFilterForSearch),
+		zap.Int32("page", pageForSearch))
+
+	// increment counter
+	searchReqCount.Inc()
+	products := make([]*pb.ResponseResult, 0)
+
+	searchRes, err := meiliclient.Index("products").Search(productNameForSearch,
+		&meilisearch.SearchRequest{
+			Limit: 25,
+		})
+	if err != nil {
+		logger.Error("failed to search in some reasons.", zap.Error(err))
+		return &pb.SearchResponse{TotalNum: 0, Response: products}, err
+	}
+
+	for _, val := range searchRes.Hits {
+		if s, ok := val.(*pb.ResponseResult); ok {
+			products = append(products, s)
+		} else {
+			logger.Error("Value is not of type pb.ResponseResult")
 		}
 	}
 
-	// create searchQuery
-	searchSQL, countSQL := createSearchSQL(productNameForSearch, sellerIDs, exhibitionDateForSearch, updateDateForSearch, categoryForSearch, rankingFilterForSearch, pageForSearch)
-
-	items, productErr := getProductInfo(db, searchSQL)
-	if productErr != nil {
-		log.Printf("Search ERROR : %s\n ", productErr)
-		return nil, productErr
-	}
-
-	total, totalErr := getTotalInfo(db, countSQL)
-	if totalErr != nil {
-		log.Printf("Search total ERROR : %s\n ", totalErr)
-		return nil, totalErr
-	}
-
-	log.Printf("Items : %v || Total :  %v \n", items, total)
+	// increment counter
 	searchResCount.Inc()
-	countSends()
-	return &pb.SearchResponse{TotalNum: total, Response: items}, nil
+
+	return &pb.SearchResponse{TotalNum: int32(len(products)), Response: products}, nil
 }
 
 func main() {
-	//set-up logging lib.
-	var logger *zap.Logger
+	// set-up logging environment using zap
 	var err error
 
-	environment := os.Getenv("GO_ENV")
+	environment := os.Getenv("MOCKTEN_ENV")
 
 	if environment == "development" || environment == "" {
 		config := zap.NewDevelopmentConfig()
@@ -104,204 +104,57 @@ func main() {
 	}
 
 	if err != nil {
-		log.Print("can't implement zap in search component.")
+		log.Println("failed to set-up zap log in searchitem. \n")
 		panic(err)
 	}
 
 	logger.Debug("this is development environment.")
-	logger.Info("well done of zap setting in search component.")
+	logger.Info("success set-up logging function.")
 
-	// clean up logger lib
 	defer logger.Sync()
 
-	// set-up gRPC receiver
+	// set-up meilisearch to register products json(documents) to index.
+	meiliclient = meilisearch.NewClient(meilisearch.ClientConfig{
+		// expect meilisearch sidecar container
+		Host:   "http://127.0.0.1:7700",
+		APIKey: os.Getenv("MEILISEARCH_MASTERKEY"),
+	})
+
+	index := meiliclient.Index("products")
+
+	// If the index 'products' does not exist, Meilisearch creates it when you first add the documents.
+	byteValue, err := os.ReadFile("/opt/etc/products.json")
+	if err != nil {
+		logger.Error("failed to load search json file", zap.Error(err))
+		panic(err)
+	}
+
+	// decode JSON to struct which is defeined this file.
+	var products []*pb.ResponseResult
+	json.Unmarshal(byteValue, &products)
+
+	task, err := index.AddDocuments(products)
+	if err != nil {
+		logger.Error("failed to add document to meilesearch task.", zap.Error(err))
+		panic(err)
+	}
+
+	logger.Info("success to execute meilisearch", zap.Int64("taskid", task.TaskUID))
+
+	// expose /metrics endpoint for observer(by default Prometheus).
+	go exportMetrics()
+
+	// start application
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
-		logger.Error("failed to listen gRPC port in search component.")
-		panic(err)
+		logger.Error("failed to set-up port listen with gRPC.")
 	}
-
 	grpcserver := grpc.NewServer()
-	pb.RegisterSearchServer(grpcserver, &server{})
+	pb.RegisterSearchItemsServer(grpcserver, &server{})
 	if err := grpcserver.Serve(lis); err != nil {
-		logger.Error("failed to register search server.")
+		logger.Error("failed to set-up application server.")
 		panic(err)
 	}
-
-	// set-up to connect Database.
-	db, err := connectDB()
-	if err != nil {
-		logger.Error("failed to connect database server from search component.")
-		panic(err)
-	}
-
-	defer db.Close()
-
-	// set-up to connect cache server.
-	// TODO
-
-	// set-up to send metrics to metlics server(prometheus by default).
-	exportMetrics()
-}
-
-var (
-	// prometheus metrics name.
-	searchReqCount = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "search_request",
-		Help: "Total number of requests that have come to search component",
-	})
-
-	searchResCount = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "search_response",
-		Help: "Total number of response that send from serch component",
-	})
-)
-
-func connectDB() (*sql.DB, error) {
-	// generally, get environment value from k8s secret resources.
-	user := os.Getenv("SECRET_USER")
-	pass := os.Getenv("SECRET_PASS")
-	sdb := os.Getenv("SECRET_DB")
-	table := os.Getenv("SECRET_TABLE")
-
-	mySQLHost := fmt.Sprintf("%s:%s@tcp(%s)/%s", user, pass, sdb, table)
-	db, err := sql.Open("mysql", mySQLHost)
-	if err = db.Ping(); err != nil {
-		logger.Error("failed to connect database server from search component.")
-		return nil, err
-	}
-
-	return db, nil
-}
-
-func getSellerID(sellerName string, db *sql.DB) ([]string, error) {
-	sellerIDs := make([]string, 0)
-	searchSellerQuery := "SELECT seller_id FROM SELLER_INFO WHERE seller_name LIKE '%" + sellerName + "%';"
-	sellerRows, err := db.Query(searchSellerQuery)
-	if err != nil {
-		log.Printf("DB Seller Query Error: %v | Seller Query is: %v ", err, searchSellerQuery)
-		return nil, err
-	}
-	defer sellerRows.Close()
-
-	var sellerID string
-	for sellerRows.Next() {
-		if err := sellerRows.Scan(&sellerID); err != nil {
-			log.Printf("Search Seller Scan Error: %v", err)
-			return nil, err
-		}
-		sellerIDs = append(sellerIDs, sellerID)
-	}
-	return sellerIDs, nil
-}
-
-func createSearchSQL(productNameForSearch string,
-	sellerIDs []string,
-	exhibitionDateForSearch string,
-	updateDateForSearch string,
-	categoryForSearch string,
-	rankingFilterForSearch int32,
-	pageForSearch int32) (string, string) {
-	baseSQL := "SELECT product_id,product_name,seller_name,stock,price,image_path,comment,category FROM PRODUCT_INFO_VIEW WHERE PRODUCT_NAME LIKE '%" + productNameForSearch + "%'"
-	baseCountSQL := "SELECT COUNT(*) FROM PRODUCT_INFO WHERE PRODUCT_NAME LIKE '%" + productNameForSearch + "%'"
-	// SearchItem all
-	if len(sellerIDs) > 0 {
-		sellerNameQuery := "("
-		for i, sellerID := range sellerIDs {
-			if i != 0 {
-				sellerNameQuery = sellerNameQuery + ","
-			}
-			sellerNameQuery = sellerNameQuery + "'" + sellerID + "'"
-		}
-		sellerNameQuery = sellerNameQuery + ")"
-		baseSQL = baseSQL + " AND SELLER_ID IN " + sellerNameQuery
-		baseCountSQL = baseCountSQL + " AND SELLER_ID IN " + sellerNameQuery
-	}
-	if len(exhibitionDateForSearch) > 0 {
-		baseSQL = baseSQL + " AND TIME=" + exhibitionDateForSearch
-		baseCountSQL = baseCountSQL + " AND TIME=" + exhibitionDateForSearch
-	}
-	if len(updateDateForSearch) > 0 {
-		baseSQL = baseSQL + " AND UPDATE_TIME=" + updateDateForSearch
-		baseCountSQL = baseCountSQL + " AND UPDATE_TIME=" + updateDateForSearch
-	}
-	if categoryForSearch != "99" {
-		baseSQL = baseSQL + " AND CATEGORY='" + categoryForSearch + "'"
-		baseCountSQL = baseCountSQL + " AND CATEGORY='" + categoryForSearch + "'"
-	}
-
-	// attach sortString
-	sortStr := CreateSortStr(rankingFilterForSearch)
-	baseSQL = baseSQL + sortStr
-
-	pagedata := (10 * pageForSearch) - 10
-	baseSQL = baseSQL + " LIMIT " + strconv.Itoa(int(pagedata)) + ",10"
-	baseSQL = baseSQL + ";"
-	baseCountSQL = baseCountSQL + ";"
-
-	log.Printf("basesQL : " + baseSQL)
-	log.Printf("baseCountSQL : " + baseCountSQL)
-
-	return baseSQL, baseCountSQL
-}
-
-func CreateSortStr(filter int32) string {
-	switch filter {
-	case 1:
-		return " ORDER BY price DESC"
-	case 2:
-		return " ORDER BY price ASC"
-	case 3:
-		return " ORDER BY rate ASC"
-	case 4:
-		return " ORDER BY update_date ASC"
-	case 5:
-		return ""
-	}
-	return ""
-}
-
-func getProductInfo(db *sql.DB, searchSQL string) ([]*pb.ResponseResult, error) {
-
-	items := make([]*pb.ResponseResult, 0)
-	rows, err := db.Query(searchSQL)
-	if err != nil {
-		log.Printf("Search Product Query Error: %v", err)
-		return nil, err
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var responseResult *pb.ResponseResult = &pb.ResponseResult{}
-		if err := rows.Scan(&responseResult.ProductId, &responseResult.ProductName, &responseResult.SellerName, &responseResult.Stocks, &responseResult.Price, &responseResult.ImageUrl, &responseResult.Comment, &responseResult.Category); err != nil {
-			log.Printf("Search Scan Error: %v | Query is: %v", err, searchSQL)
-			return items, err
-		}
-		items = append(items, responseResult)
-		log.Printf("items is : %v", items)
-	}
-
-	return items, nil
-}
-
-func getTotalInfo(db *sql.DB, countSQL string) (int32, error) {
-	var total int32
-
-	rows, err := db.Query(countSQL)
-	if err != nil {
-		log.Printf("Count Query Error: %v | Query is: %v", err, countSQL)
-		return 0, err
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		if err := rows.Scan(&total); err != nil {
-			log.Printf("Count Scan Error: %v | Query is: %v", err, countSQL)
-			return 0, err
-		}
-	}
-
-	return total, nil
 }
 
 // for goroutin
