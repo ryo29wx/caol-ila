5dea01f2bf9dedafa0086d6b457ac5cf8c4a3b01
ryo29wx <59174187+ryo29wx@users.noreply.github.com>
2024-01-10 21:17:30 +0900

    small fix tools

--- tools/generateJson.go
+++ tools/generateJson.go
@@ -2,55 +2,72 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
+
+	"github.com/google/uuid"
 )
 
 // UserProfile はユーザーのプロファイル情報を表す構造体です。
+/*
+{"user_id": "string",
+ "nick_name": "string",
+ "sex": int, //1=man, 2=wemen, 3=other
+ "age": int,
+ "job_title": "string",
+ "company": "string",
+ "likes": ["string", "string", ... ],
+ "dislikes": ["string", "string", ... ],
+ "blocks": ["string", "string", ... ],
+ "main_image": "string",
+ "image_path": ["string", "string", ... ],
+ "regist_day": timestamp,
+ "last_login": timestamp}
+}
+*/
 type UserProfile struct {
-	UserID       string   `json:"user_id"`
-	UserName     string   `json:"user_name"`
-	Title        string   `json:"title"`
-	Company      string   `json:"company"`
-	LikeUsers    []string `json:"like_users"`
-	BlockUsers   []string `json:"block_users"`
-	Career       []Career `json:"career"`
-	ChatIDList   []string `json:"chat_id_list"`
-	Description  string   `json:"description"`
-	ImageURLList []string `json:"image_url_list"`
-	Confirmation bool     `json:"confirmation"`
-	LastLogin    string   `json:"last_login"`
-	CreateDate   string   `json:"create_date"`
-	Billing      []int    `json:"billing"`
+	UserID    string   `json:"user_id"`
+	UserName  string   `json:"user_name"`
+	Sex       int      `json:"sex"`
+	Age       int      `json:"age"`
+	JobTitle  string   `json:"job_title"`
+	Company   string   `json:"company"`
+	Likes     []string `json:"likes"`
+	Dislikes  []string `json:"dislikes"`
+	Blocks    []string `json:"blocks"`
+	MainImage string   `json:"main_image"`
+	ImagePath []string `json:"image_path"`
+	RegistDay string   `json:"regist_day"`
+	LastLogin string   `json:"last_login"`
 }
 
-// Career はユーザーのキャリア情報を表す構造体です。
-type Career struct {
-	Position string `json:"役職"`
-	Years    string `json:"年数"`
-	// その他のキャリア関連情報
-}
-
+// 550e8400-e29b-41d4-a716-446655440000
 func main() {
+	n := flag.Int("n", 10, "How many jsons which you want to create")
 	// UserProfile構造体のインスタンスを作成し、データを埋めます。
-	profile := UserProfile{
-		UserID:       "user123",
-		UserName:     "山田太郎",
-		Title:        "エンジニア",
-		Company:      "株式会社サンプル",
-		LikeUsers:    []string{"user456", "user789"},
-		BlockUsers:   []string{"user111", "user222"},
-		Career:       []Career{{Position: "マネージャー", Years: "5年"}},
-		ChatIDList:   []string{"chat123", "chat456"},
-		Description:  "こんにちは、山田太郎です。",
-		ImageURLList: []string{"http://example.com/image1.jpg", "http://example.com/image2.jpg"},
-		Confirmation: true,
-		LastLogin:    "2021-01-01T12:00:00Z",
-		CreateDate:   "2020-01-01T00:00:00Z",
-		Billing:      []int{1, 2, 3},
+	var result []UserProfile
+	for i := 0; i < *n; i++ {
+		profile := UserProfile{
+			UserID:    generateUUID(),
+			UserName:  "山田太郎",
+			Sex:       1,
+			Age:       20,
+			JobTitle:  "hoge",
+			Company:   "株式会社サンプル",
+			Likes:     []string{"550e8400-e29b-41d4-a716-446655440001", "550e8400-e29b-41d4-a716-446655440002"},
+			Dislikes:  []string{"550e8400-e29b-41d4-a716-446655440003", "550e8400-e29b-41d4-a716-446655440004"},
+			Blocks:    []string{"550e8400-e29b-41d4-a716-446655440005", "550e8400-e29b-41d4-a716-446655440006"},
+			MainImage: "http://sample.com/image01",
+			ImagePath: []string{"http://sample.com/image02", "http://sample.com/image03"},
+			RegistDay: "hoge",
+			LastLogin: "fuga",
+		}
+
+		result = append(result, profile)
 	}
 
 	// JSONにエンコードします。
-	jsonData, err := json.MarshalIndent(profile, "", "  ")
+	jsonData, err := json.MarshalIndent(result, "", "  ")
 	if err != nil {
 		fmt.Println("Error encoding JSON:", err)
 		return
@@ -58,4 +75,13 @@ func main() {
 
 	// エンコードしたJSONを出力します。
 	fmt.Println(string(jsonData))
+}
+
+func generateUUID() string {
+	u, err := uuid.NewRandom()
+	if err != nil {
+		fmt.Println(err)
+		return "550e8400-e29b-41d4-a716-446655440000"
+	}
+	return u.String()
 }
