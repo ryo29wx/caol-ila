47b53833ca1a26cb6331e15c0d231ec14a83e33e
ryo29wx <59174187+ryo29wx@users.noreply.github.com>
2024-06-06 21:41:38 +0900

    small fix UI

--- components/front-service/src/pages/AdminLogin.tsx
+++ components/front-service/src/pages/AdminLogin.tsx
@@ -1,53 +1,123 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Container, TextField, Button, Grid, Typography } from '@mui/material';
+import { Box, Container, TextField, Button, IconButton, InputAdornment, Grid, Typography } from '@mui/material';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 const AdminLoginPage: React.FC = () => {
-    const [password, setPassword] = useState('');
-    const [userID, setUserID] = useState('');
     const navigate = useNavigate();
+    const [showPassword, setShowPassword] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
+    const [admin, setAdmin] = useState({
+      name: '',
+      password: ''
+    });
 
-    const handleLogin = (e: FormEvent) => {
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setAdmin({ ...admin, [e.target.name]: e.target.value });
+    };
+
+    const handleClickShowPassword = () => {
+      setShowPassword(!showPassword);
+    };
+
+    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+      event.preventDefault();
+    };
+
+    const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        
-        if (password == "1234" ) {
-            navigate('/admin');
-        } else {
-            alert('Invalid username or password');
+
+        // const apiUrl = process.env.REACT_APP_ADMIN_API;
+        const apiUrl = 'http://localhost:8080';
+
+        try {
+
+          console.log(admin); // Success handling
+          const response = await fetch(`${apiUrl}/v1/admin/login`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(admin)
+          });
+  
+          if (!response.ok) {
+            setErrorMessage('Passwords or Name do not match');
+            throw new Error('Something went wrong');
+          }
+  
+          const apiStatus = await response.json();
+          console.log(apiStatus); // Success handling
+          navigate('/admin');
+          
+        } catch (error) {
+          console.error(error); // Error handling
         }
     };
 
     return (
         <Container maxWidth="sm">
+        <Box component="form" onSubmit={handleLogin} noValidate sx={{ mt: 1 }}>
         <Grid container spacing={3} direction="column" alignItems="center" justifyContent="center" style={{ minHeight: '100vh' }}>
           <Grid item>
             <Typography variant="h4">Admin Login</Typography>
           </Grid>
           <Grid item>
             <TextField
-              label="Admin ID"
-              variant="outlined"
+              margin="normal"
+              required
               fullWidth
-              value={userID}
-              onChange={(e) => setUserID(e.target.value)}
-            />
+              id="name"
+              label="Name"
+              name="name"
+              autoComplete="name"
+              autoFocus
+              onChange={handleChange}
+              />
           </Grid>
           <Grid item>
             <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="password"
               label="Password"
               variant="outlined"
-              type="password"
-              fullWidth
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              autoComplete="password"
+              name="password"
+              type={showPassword ? 'text' : 'password'}
+              onChange={handleChange}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
             />
           </Grid>
           <Grid item>
-            <Button variant="contained" color="primary" onClick={handleLogin} fullWidth>
-              Admin Login
+            <Button type="submit" variant="contained" color="primary" fullWidth>
+              Login
             </Button>
           </Grid>
+          {errorMessage && (
+          <Grid item>
+            <Typography variant="body2" color="error">
+              {errorMessage}
+            </Typography>
+          </Grid>
+        )}
         </Grid>
+        </Box>
       </Container>
     );
 };
--- components/front-service/src/pages/FavoriteGetList.tsx
+++ components/front-service/src/pages/FavoriteGetList.tsx
@@ -1,13 +1,87 @@
 import React from 'react';
 import DashboardBottomNavigation from '../components/DashboardBottomNavigation';
 import Appbar from '../components/Appbar';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+
 
 const FavoriteGetList: React.FC = () => {
 
     return (
         <div>
             <Appbar />
-            <h1> Maintenance Now. </h1>
+                <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                    <ListItem alignItems="flex-start">
+                        <ListItemAvatar>
+                        <Avatar alt="Remy Sharp" src="/images/TestUser1.jpeg" />
+                        </ListItemAvatar>
+                        <ListItemText
+                        primary="Brunch this weekend?"
+                        secondary={
+                            <React.Fragment>
+                            <Typography
+                                sx={{ display: 'inline' }}
+                                component="span"
+                                variant="body2"
+                                color="text.primary"
+                            >
+                                Ali Connors
+                            </Typography>
+                            {" — I'll be in your neighborhood doing errands this…"}
+                            </React.Fragment>
+                        }
+                        />
+                    </ListItem>
+                    <Divider variant="inset" component="li" />
+                    <ListItem alignItems="flex-start">
+                        <ListItemAvatar>
+                        <Avatar alt="Travis Howard" src="/images/TestUser2.jpeg" />
+                        </ListItemAvatar>
+                        <ListItemText
+                        primary="Summer BBQ"
+                        secondary={
+                            <React.Fragment>
+                            <Typography
+                                sx={{ display: 'inline' }}
+                                component="span"
+                                variant="body2"
+                                color="text.primary"
+                            >
+                                to Scott, Alex, Jennifer
+                            </Typography>
+                            {" — Wish I could come, but I'm out of town this…"}
+                            </React.Fragment>
+                        }
+                        />
+                    </ListItem>
+                    <Divider variant="inset" component="li" />
+                    <ListItem alignItems="flex-start">
+                        <ListItemAvatar>
+                        <Avatar alt="Cindy Baker" src="/images/TestUser3.jpeg" />
+                        </ListItemAvatar>
+                        <ListItemText
+                        primary="Oui Oui"
+                        secondary={
+                            <React.Fragment>
+                            <Typography
+                                sx={{ display: 'inline' }}
+                                component="span"
+                                variant="body2"
+                                color="text.primary"
+                            >
+                                Sandra Adams
+                            </Typography>
+                            {' — Do you have Paris recommendations? Have you ever…'}
+                            </React.Fragment>
+                        }
+                        />
+                    </ListItem>
+                </List>
             <DashboardBottomNavigation />
         </div>
     );
