fb09b22e84a2fb6a5d84af654b77d9f7eb88e00f
Ryo Kiuchi <ryokiuchi@RyonoMacBook-Air.local>
2024-10-14 20:38:20 +0900

    fix/ admin components

--- components/front-service/src/pages/AdminCreateAccount.tsx
+++ components/front-service/src/pages/AdminCreateAccount.tsx
@@ -1,62 +1,83 @@
-import React, { useState, ChangeEvent} from 'react';
-import { Button, TextField, Container, Box } from '@mui/material';
-
-type ImageUploadState = {
-    file: File | null;
-    previewUrl: string | null;
-};
+import React, { useState } from 'react';
+import { Box, Container, TextField, Button, IconButton, InputAdornment, Grid, Typography } from '@mui/material';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 const AdminCreateAccount = () => {
-  const [product, setProduct] = useState({
-    product_name: '',
-    seller_name: '',
-    category: 0,
-    price: 0,
-    stocks: 0,
-    main_image: '',
-    images: ['', ''],
-    token: ''
+
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [email, setEmail] = useState('');
+
+  const validateEmail = (email: string) => {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
+  };
+  
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+  
+  const handleClickShowConfirmPassword = () => {
+    setShowConfirmPassword(!showConfirmPassword);
+  };
+  
+  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
+
+  const [user, setUser] = useState({
+    first_name: '',
+    last_name: '',
+    password: '',
+    email: '',
+    phone_num: '',
+    postcode: '',
+    address1: '',
+    address2: '',
+    address3: '',
+    birthday: ''
   });
   
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setProduct({ ...product, [e.target.name]: e.target.value });
+    setUser({ ...user, [e.target.name]: e.target.value });
   };
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const apiUrl = process.env.REACT_APP_ADDITEM_API;
+     // const apiUrl = process.env.REACT_APP_ADDITEM_API;
+    const apiUrl = "http://localhost:50051";
+ 
+    if (password !== confirmPassword) {
+      setErrorMessage('Passwords do not match');
+    } else if (!validateEmail(email)) {
+      setErrorMessage('Invalid email address'); 
+    } else {
+      setErrorMessage('');
+      user.password = password;
+      user.email = email;
+      try {
+        const response = await fetch(`${apiUrl}/v1/admin/account/create`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(user)
+        });
 
-    try {
-      const response = await fetch(`${apiUrl}/v1/admin/account/create`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        },
-        body: JSON.stringify(product)
-      });
+        if (!response.ok) {
+          throw new Error('Something went wrong');
+        }
 
-      if (!response.ok) {
-        throw new Error('Something went wrong');
+        const data = await response.json();
+        console.log(data); // Success handling
+      } catch (error) {
+        console.error(error); // Error handling
       }
-
-      const data = await response.json();
-      console.log(data); // Success handling
-    } catch (error) {
-      console.error(error); // Error handling
-    }
-  };
-
-  const [imageUpload, setImageUpload] = useState<ImageUploadState>({ file: null, previewUrl: null });
-
-  const handleImageChange = (event: ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files && event.target.files[0]) {
-      const file = event.target.files[0];
-
-      setImageUpload({
-        file: file,
-        previewUrl: URL.createObjectURL(file),
-      });
     }
   };
 
@@ -68,50 +89,98 @@ const AdminCreateAccount = () => {
           margin="normal"
           required
           fullWidth
-          id="user_name"
-          label="User Name"
-          name="user_name"
-          autoComplete="user_name"
+          id="first_name"
+          label="First Name"
+          name="first_name"
+          autoComplete="first_name"
+          autoFocus
+          onChange={handleChange}
+        />
+       <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="last_name"
+          label="Last Name"
+          name="last_name"
+          autoComplete="last_name"
           autoFocus
           onChange={handleChange}
         />
         <TextField
           margin="normal"
+          label="Password"
+          required
+          variant="outlined"
+          type={showPassword ? 'text' : 'password'}
           fullWidth
-          id="mail_address1"
-          label="Main Mail Address"
-          name="mail_address1"
-          autoComplete="mail_address1"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+        <TextField
+          margin="normal"
+          label="Confirm Password"
+          required
+          variant="outlined"
+          type={showConfirmPassword ? 'text' : 'password'}
+          fullWidth
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle confirm password visibility"
+                  onClick={handleClickShowConfirmPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showConfirmPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+        {errorMessage && (
+          <Grid item>
+            <Typography variant="body2" color="error">
+              {errorMessage}
+            </Typography>
+          </Grid>
+        )}
+        <TextField
+          margin="normal"
+          fullWidth
+          required
+          id="mail_address"
+          label="Mail Address"
+          name="mail_address"
+          autoComplete="mail_address"
           autoFocus
-          onChange={handleChange}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <TextField
           margin="normal"
           fullWidth
-          id="mail_address2"
-          label="Mail Address 2"
-          name="mail_address2"
-          autoComplete="mail_address2"
-          autoFocus
-          onChange={handleChange}
-        />
-        <TextField
-          margin="normal"
-          fullWidth
-          id="mail_address3"
-          label="Mail Address 3"
-          name="mail_address3"
-          autoComplete="mail_address3"
-          autoFocus
-          onChange={handleChange}
-        />
-        <TextField
-          margin="normal"
-          fullWidth
-          id="phone_number1"
-          label="Main Phone Number"
-          name="phone_number1"
-          autoComplete="phone_number1"
+          required
+          id="phone_num"
+          label="Phone Number"
+          name="phone_num"
+          autoComplete="phone_num"
           autoFocus
           type="number"
           onChange={handleChange}
@@ -119,21 +188,11 @@ const AdminCreateAccount = () => {
         <TextField
           margin="normal"
           fullWidth
-          id="phone_number2"
-          label="Phone Number 2"
-          name="phone_number2"
-          autoComplete="phone_number2"
-          autoFocus
-          type="number"
-          onChange={handleChange}
-        />
-        <TextField
-          margin="normal"
-          fullWidth
-          id="phone_number3"
-          label="Phone Number 3"
-          name="phone_number3"
-          autoComplete="phone_number3"
+          required
+          id="postcode"
+          label="Postcode"
+          name="postcode"
+          autoComplete="postcode"
           autoFocus
           type="number"
           onChange={handleChange}
@@ -163,6 +222,7 @@ const AdminCreateAccount = () => {
         <TextField
           margin="normal"
           fullWidth
+          required
           id="address3"
           label="Address(Street)"
           name="address3"
@@ -171,32 +231,19 @@ const AdminCreateAccount = () => {
           onChange={handleChange}
         />
         <TextField
-          margin="normal"
+          id="birthday"
+          label="Birthday"
+          required
+          type="date"
           fullWidth
-          id="postcode"
-          label="Postcode"
-          name="postcode"
-          autoComplete="postcode"
-          autoFocus
-          type="number"
+          InputLabelProps={{
+            shrink: true, 
+          }}
+          autoComplete="Birthday"
           onChange={handleChange}
+          
         />
-        <div>
-          <input
-            accept="image/*"
-            style={{ display: 'none' }}
-            id="raised-button-file"
-            multiple
-            type="file"
-            onChange={handleImageChange}
-          />
-          <label htmlFor="raised-button-file">
-            <Button variant="contained" component="span">
-              Upload Image
-            </Button>
-          </label>
-          {imageUpload.previewUrl && <img src={imageUpload.previewUrl} alt="Preview" style={{ width: '100%', marginTop: '10px' }} />}
-        </div>
+        
         <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
           Create
         </Button>
