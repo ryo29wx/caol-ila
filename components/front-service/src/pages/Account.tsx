9e81b705610fbaa39919234887ad9d3bafcccefc
Ryo Kiuchi <ryokiuchi@RyonoMacBook-Air.local>
2024-11-03 00:27:32 +0900

    fix login funtion

--- components/front-service/src/pages/Account.tsx
+++ components/front-service/src/pages/Account.tsx
@@ -1,56 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import DashboardBottomNavigation from '../components/DashboardBottomNavigation';
-import Appbar from '../components/Appbar';
-import axios from 'axios';
-import { Container, Typography, CircularProgress } from '@mui/material';
+import React from 'react';
+import keycloak from './../Keycloak';
 
-interface MyAccountData {
-    id: number;
-    name: string;
-    email: string;
-}
+const UserInfo: React.FC = () => {
+  const handleLogout = () => {
+    keycloak.logout();
+  };
 
-const Account: React.FC = () => {
-
-    const [data, setData] = useState<MyAccountData | null>(null);
-    const [loading, setLoading] = useState<boolean>(true);
-    const [error, setError] = useState<string | null>(null);
-  
-    useEffect(() => {
-      // APIリクエストを送信
-      axios.get<MyAccountData>('https://api.example.com/user/1')
-        .then(response => {
-          setData(response.data);
-          setLoading(false);
-        })
-        .catch(error => {
-          setError(error.message);
-          setLoading(false);
-        });
-    }, []);
-  
-    if (loading) {
-      return <CircularProgress />;
-    }
-  
-    if (error) {
-      return <Typography color="error">{error}</Typography>;
-    }
-  
-    return (
-      <Container>
-      <Appbar />
-        <Typography variant="h4">User Details</Typography>
-        {data && (
-          <>
-            <Typography variant="body1">ID: {data.id}</Typography>
-            <Typography variant="body1">Name: {data.name}</Typography>
-            <Typography variant="body1">Email: {data.email}</Typography>
-          </>
-        )}
-      <DashboardBottomNavigation />
-      </Container>
-    );
+  return (
+    <div>
+      <h1>Welcome, {keycloak.tokenParsed?.preferred_username}</h1>
+      <button onClick={handleLogout}>Logout</button>
+    </div>
+  );
 };
 
-export default Account;
+export default UserInfo;
--- components/front-service/src/pages/Login.tsx
+++ components/front-service/src/pages/Login.tsx
@@ -1,137 +1,19 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Box, Container, TextField, Button, IconButton, InputAdornment, Grid, Typography } from '@mui/material';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import React from 'react';
+import keycloak from './../Keycloak';
 
-const LoginPage: React.FC = () => {
-    const navigate = useNavigate();
-    const [showPassword, setShowPassword] = useState(false);
-    const [errorMessage, setErrorMessage] = useState('');
-    const [user, setUser] = useState({
-      mail: '',
-      password: ''
-    });
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setUser({ ...user, [e.target.name]: e.target.value });
-    };
+const Login: React.FC = () => {
+  const handleLogin = () => {
+    keycloak.login();
+  };
 
-    const handleClickShowPassword = () => {
-      setShowPassword(!showPassword);
-    };
-
-    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
-      event.preventDefault();
-    };
-
-    const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-
-        // const apiUrl = process.env.REACT_APP_ADMIN_API;
-        const apiUrl = 'http://localhost:8080';
-
-        try {
-
-          console.log(user); // Success handling
-          const response = await fetch(`${apiUrl}/v1/login`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(user)
-          });
-  
-          if (!response.ok) {
-            setErrorMessage('Passwords or Name do not match');
-            throw new Error('Something went wrong');
-          }
-  
-          const apiStatus = await response.json();
-          console.log(apiStatus); // Success handling
-          navigate('/');
-          
-        } catch (error) {
-          console.error(error); // Error handling
-        }
-    };
-
-    const handleCreateAccount = () => {
-        navigate('/account/create');
-    };
-
-    return (
-        <Container maxWidth="sm">
-        <Box component="form" onSubmit={handleLogin} noValidate sx={{ mt: 1 }}>
-        <Grid container spacing={3} direction="column" alignItems="center" justifyContent="center" style={{ minHeight: '100vh' }}>
-          <Grid item>
-            <Typography variant="h4">Caol-Ila Login</Typography>
-          </Grid>
-          <Grid item>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="userid"
-              label="Mail Address"
-              name="userid"
-              autoComplete="userid"
-              autoFocus
-              onChange={handleChange}
-              />
-          </Grid>
-          <Grid item>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="password"
-              label="Password"
-              variant="outlined"
-              autoComplete="password"
-              name="password"
-              type={showPassword ? 'text' : 'password'}
-              onChange={handleChange}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}
-                      edge="end"
-                    >
-                      {showPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </Grid>
-          <Grid item>
-            <Button type="submit" variant="contained" color="primary" fullWidth>
-              Login
-            </Button>
-          </Grid>
-          {errorMessage && (
-          <Grid item>
-            <Typography variant="body2" color="error">
-              {errorMessage}
-            </Typography>
-          </Grid>
-          )}
-        </Grid>
-        </Box>
-        <Box component="form" onSubmit={handleCreateAccount} noValidate sx={{ mt: 1 }}>
-            <h4>初めてアカウントを作る場合はこちら！</h4>
-            <Grid item>
-            <Button type="submit" variant="contained" color="secondary" fullWidth>
-                Create Account
-            </Button>
-            </Grid>
-        </Box>
-      </Container>
-    );
+  return (
+    <div>
+      <h2>Welcom to Caol-Ila!!</h2>
+      <h3>Let's create your world!</h3>
+      <button onClick={handleLogin}>Login / Create your account</button>
+    </div>
+  );
 };
 
-export default LoginPage;
+export default Login;
